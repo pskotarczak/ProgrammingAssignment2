be249f58fd6c8def3fe58f558e8b872b56a7e795
pskotarczak <pskotarczak@users.noreply.github.com>
2014-05-26 00:48:42 +0200

    final commit befor submitting the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -13,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
         i <<- NULL
     }
     get <- function() x
-    setinv <- function(inverse) i <- solve
+    setinv <- function(inverse) i <- inverse
     getinv <- function i
     list(set = set, get = get,
          setinv = setinv, geinv = getinv)
